67f9bfb5fb438be98bbea5d18db8c85828f46deb
RibhuMitra <ribhumitra30@gmail.com>
2025-03-05 03:03:11 +0530

    Initial Commit

---
+++ app/(root)/(tabs)/expore.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const expore = () => {
+  return (
+    <View>
+      <Text>Expore</Text>
+    </View>
+  )
+}
+
+export default expore
